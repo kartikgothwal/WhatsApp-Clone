e4f53600195728a0053baa312519ca531475b260
Kartik Gothwal <kartikgothwal28@gmail.com>
2023-09-19 03:17:20 +0530

    FireBase auth done

--- src/components/UserMainPage.jsx
+++ src/components/UserMainPage.jsx
@@ -228,143 +228,149 @@ const UserMainPage = () => {
               </div>
             </section>
             <section
-              className="h-full relative grid grid-rows-[3.67rem_repeat(1,1fr)_4rem] "
+              className="h-full relative"
               style={{
                 background: "#efeae2",
               }}
             >
-              <header
-                className=" z-100  grid grid-cols-[7rem__repeat(1,1fr)_7rem]"
-                style={{
-                  background: "#f0f2f5",
-                }}
-              >
-                <figure className=" pl-[2rem] w-[5rem] h-full flex items-center justify-center">
-                  <img
-                    src={Friend.profileImg}
-                    alt="user_img"
-                    className="rounded-[40px] h-[2.8rem]"
-                  />
-                </figure>
-                <div className="flex flex-col justify-center ">
-                  {Friend.name && <h1>{Friend.name}</h1>}
-                  <p className="text-gray-500 text-sm">Today</p>
-                </div>
-                <div className="flex justify-end items-center h-full w-full pr-6 gap-10">
-                  <DuoIcon
+              {Friend.id ? (
+                <div className="h-full relative grid grid-rows-[3.67rem_repeat(1,1fr)_4rem] ">
+                  <header
+                    className=" z-100 grid grid-cols-[7rem__repeat(1,1fr)_7rem]"
                     style={{
-                      color: "#54656f",
-                      // color: "#ffffff",
-                      fontSize: "40px",
-                      cursor: "pointer",
-                      border: "1px solid #54656f",
-                      padding: "4px 15px ",
-                      width: "5rem",
-                      borderRadius: "20px",
+                      background: "#f0f2f5",
                     }}
-                  />
-                  <SearchIcon
-                    style={{
-                      color: "#54656f",
-                      // color: "#ffffff",
-                      fontSize: "25px",
-                      cursor: "pointer",
-                    }}
-                  />
-                  <MoreVertIcon
-                    style={{
-                      color: "#54656f",
-                      // color: "#ffffff",
-                      fontSize: "25px",
-                      cursor: "pointer",
-                    }}
-                  />
-                </div>
-              </header>
+                  >
+                    <figure className=" pl-[2rem] w-[5rem] h-full flex items-center justify-center">
+                      <img
+                        src={Friend.profileImg}
+                        alt="user_img"
+                        className="rounded-[40px] h-[2.8rem]"
+                      />
+                    </figure>
+                    <div className="flex flex-col justify-center ">
+                      {Friend.name && <h1>{Friend.name}</h1>}
+                      <p className="text-gray-500 text-sm">Today</p>
+                    </div>
+                    <div className="flex justify-end items-center h-full w-full pr-6 gap-10">
+                      <DuoIcon
+                        style={{
+                          color: "#54656f",
+                          // color: "#ffffff",
+                          fontSize: "40px",
+                          cursor: "pointer",
+                          border: "1px solid #54656f",
+                          padding: "4px 15px ",
+                          width: "5rem",
+                          borderRadius: "20px",
+                        }}
+                      />
+                      <SearchIcon
+                        style={{
+                          color: "#54656f",
+                          // color: "#ffffff",
+                          fontSize: "25px",
+                          cursor: "pointer",
+                        }}
+                      />
+                      <MoreVertIcon
+                        style={{
+                          color: "#54656f",
+                          // color: "#ffffff",
+                          fontSize: "25px",
+                          cursor: "pointer",
+                        }}
+                      />
+                    </div>
+                  </header>
 
-              <main className="relative w-full   bg-center  bg-no-repeat bg-cover">
-                <div className="absolute w-full top-0  opacity-[0.4] left-0 custom-background h-full overflow-hidden"></div>
+                  <main className="relative w-full   bg-center  bg-no-repeat bg-cover">
+                    <div className="absolute w-full top-0  opacity-[0.4] left-0 custom-background h-full overflow-hidden"></div>
 
-                <div className="relative h-full w-full px-10 py-2">
-                  <div className="relative flex items-end flex-col gap-2 h-full w-full">
-                    {Messages.map((messageData, index) => {
-                      return (
-                        <div
-                          key={index}
-                          role="row"
-                          className={`relative z-1000 w-full flex ${
-                            messageData.id == Friend.ID ? "" : "justify-end"
-                          }`}
-                        >
-                          <span
-                            className="opacity-100 h-full text-sm inline-block relative px-5 py-1 rounded-md bg-orange-300"
-                            style={{
-                              backgroundColor: "#fefffe",
-                              color: "#111b21",
-                            }}
-                          >
-                            {messageData.message}
-                          </span>
-                        </div>
-                      );
-                    })}
+                    <div className="relative h-full w-full px-10 py-2">
+                      <div className="relative flex items-end flex-col gap-2 h-full w-full">
+                        {Messages.map((messageData, index) => {
+                          return (
+                            <div
+                              key={index}
+                              role="row"
+                              className={`relative z-1000 w-full flex ${
+                                messageData.id == Friend.ID ? "" : "justify-end"
+                              }`}
+                            >
+                              <span
+                                className="opacity-100 h-full text-sm inline-block relative px-5 py-1 rounded-md bg-orange-300"
+                                style={{
+                                  backgroundColor: "#fefffe",
+                                  color: "#111b21",
+                                }}
+                              >
+                                {messageData.message}
+                              </span>
+                            </div>
+                          );
+                        })}
+                      </div>
+                    </div>
+                  </main>
+
+                  <div
+                    className="grid grid-cols-[7rem_repeat(1,1fr)_5rem]"
+                    style={{
+                      background: "#f0f2f5",
+                    }}
+                  >
+                    <div className="flex justify-evenly items-center ">
+                      <MoodIcon
+                        style={{
+                          color: "#54656f",
+                          fontSize: "30px",
+                          cursor: "pointer",
+                        }}
+                      />
+                      <AddIcon
+                        style={{
+                          color: "#54656f",
+                          fontSize: "30px",
+                          cursor: "pointer",
+                        }}
+                      />
+                    </div>
+                    <div className=" justify-center flex items-center ">
+                      <input
+                        type="text"
+                        name="message"
+                        placeholder="Type a message"
+                        className="bg-white w-full h-11 px-5 rounded-xl outline-none text-sm text-gray-800"
+                        value={NewUserMessages.message}
+                        onChange={HandleMessageInputChange}
+                      />
+                    </div>
+                    <div className="flex items-center justify-center">
+                      {!NewUserMessages.message ? (
+                        <MicIcon
+                          style={{
+                            color: "#54656f",
+                            fontSize: "30px",
+                            cursor: "pointer",
+                          }}
+                        />
+                      ) : (
+                        <SendIcon
+                          style={{
+                            color: "#54656f",
+                            fontSize: "30px",
+                            cursor: "pointer",
+                          }}
+                          onClick={HandleSendMessaeBtn}
+                        />
+                      )}
+                    </div>
                   </div>
                 </div>
-              </main>
-
-              <div
-                className="grid grid-cols-[7rem_repeat(1,1fr)_5rem]"
-                style={{
-                  background: "#f0f2f5",
-                }}
-              >
-                <div className="flex justify-evenly items-center ">
-                  <MoodIcon
-                    style={{
-                      color: "#54656f",
-                      fontSize: "30px",
-                      cursor: "pointer",
-                    }}
-                  />
-                  <AddIcon
-                    style={{
-                      color: "#54656f",
-                      fontSize: "30px",
-                      cursor: "pointer",
-                    }}
-                  />
-                </div>
-                <div className=" justify-center flex items-center ">
-                  <input
-                    type="text"
-                    name="message"
-                    placeholder="Type a message"
-                    className="bg-white w-full h-11 px-5 rounded-xl outline-none text-sm text-gray-800"
-                    value={NewUserMessages.message}
-                    onChange={HandleMessageInputChange}
-                  />
-                </div>
-                <div className="flex items-center justify-center">
-                  {!NewUserMessages.message ? (
-                    <MicIcon
-                      style={{
-                        color: "#54656f",
-                        fontSize: "30px",
-                        cursor: "pointer",
-                      }}
-                    />
-                  ) : (
-                    <SendIcon
-                      style={{
-                        color: "#54656f",
-                        fontSize: "30px",
-                        cursor: "pointer",
-                      }}
-                      onClick={HandleSendMessaeBtn}
-                    />
-                  )}
-                </div>
-              </div>
+              ) : (
+                <div className="h-full w-full NoBackground"></div>
+              )}
             </section>
           </main>
         </section>
